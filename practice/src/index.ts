7a50bcb6c9a1da4c224b69c84b2a8b9d3f56e2dd
ryushi-masa <ohaoha.0610@gmail.com>
2024-05-11 02:08:49 +0900

    TypeScript 配列の型についての学習

--- practice/src/index.ts
+++ practice/src/index.ts
@@ -1,3 +1,17 @@
 const message: string = 'Hello World'
 
 console.log(message)
+
+const ary: Array<string> = ['test']
+console.log(ary)
+
+const ary2: Array<{ name: string, age: number }> = [
+  {
+    name: 'foo',
+    age: 18,
+  }
+]
+console.log(ary2)
+
+const ary3: readonly number[] = [1, 2, 3]
+console.log(ary3)
