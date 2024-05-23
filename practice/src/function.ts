cbf49a32cf0730260aada0ea9d0c5c09ed5e1d3d
ryushi-masa <ohaoha.0610@gmail.com>
2024-05-24 02:32:19 +0900

    型を定義して関数式を作った

--- practice/src/function.ts
+++ practice/src/function.ts
@@ -13,3 +13,20 @@ function helloWorld(): void {
 }
 
 helloWorld()
+
+type Human = {
+  height: number;
+  weight: number;
+}
+
+const calcBMI = function (human: Human): number {
+  return human.weight / human.height ** 2
+}
+
+console.log(calcBMI({height: 1.75, weight: 69}))
+
+const calcBMI2 = function (human: { weight: number, height: number}): number {
+  return human.weight / human.height ** 2
+}
+
+console.log(calcBMI2({height: 1.75, weight: 69}))
