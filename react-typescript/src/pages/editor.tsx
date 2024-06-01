9bedddb20ad50064c9c9d61a97a88d3763a661d1
ryushi-masa <ohaoha.0610@gmail.com>
2024-06-01 21:08:42 +0900

    ヘッダーをコンポーネント化

--- react-typescript/src/pages/editor.tsx
+++ react-typescript/src/pages/editor.tsx
@@ -6,23 +6,9 @@ import { putMemo } from '../indexeddb/memos'
 import { Button } from '../components/button'
 import { SaveModal } from '../components/save_modal'
 import { Link } from 'react-router-dom'
+import { Header } from '../components/header'
 
 const { useState } = React
-
-
-const Header = styled.header`
-  align-content: center;
-  display: flex;
-  font-size: 1.5rem;
-  height: 2rem;
-  left: 0;
-  line-height: 2rem;
-  justify-content: space-between;
-  padding: 0.5rem 1rem;
-  position: fixed;
-  right: 0;
-  top: 0;
-`
 
 const Wrapper = styled.div`
   bottom: 0;
@@ -55,10 +41,11 @@ const Preview = styled.div`
   width: 50vw;
 `
 
-const HeaderControl = styled.div`
-  height: 2rem;
-  display: flex;
-  align-content: center
+const HeaderArea = styled.div`
+  position: fixed;
+  right: 0;
+  top: 0;
+  left: 0;
 `
 
 const StorageKey = 'pages/editor:text'
@@ -70,17 +57,16 @@ export const Editor: React.FC = () => {
   const [showModal, setShowModal] = useState(false)
   return (
     <>
-      <Header>
-        Markdown Editor
-        <HeaderControl>
+    <HeaderArea>
+      <Header title="Markdown Editor">
           <Button onClick={() => setShowModal(true)}>
             保存する
           </Button>
           <Link to="/history">
             履歴を見る
           </Link>
-        </HeaderControl>
       </Header>
+    </HeaderArea>
       <Wrapper>
         <TextArea
           onChange={(event) => setText(event.target.value)}
