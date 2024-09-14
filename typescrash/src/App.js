d5411672c93ee5ae6e8badf53e335f2f17addfa1
ryushi-masa <ohaoha.0610@gmail.com>
2024-09-14 23:33:19 +0900

    画像表示機能を実装

--- typescrash/src/App.js
+++ typescrash/src/App.js
@@ -3,19 +3,25 @@ import Header from './components/Header';
 import Search from './components/Search';
 import Calculator from './components/Calculator';
 import unsplash from './api/unsplash';
+import ImageList from './components/ImageList'
 
 function App() {
+
+  const [images, setImages] = React.useState([]);
   const onSearchSubmit = async (term) => {
     const response = await unsplash.get('/search/photos', {
       params: { query: term }
     });
-    console.log(response);
+    setImages(response.data.results);
   }
   return (
     <div className="App">
       <Header>
         <Search onSearchSubmit={onSearchSubmit} />
         <Calculator />
+        <div>
+          <ImageList images={images} />
+        </div>
       </Header>
     </div>
   )
